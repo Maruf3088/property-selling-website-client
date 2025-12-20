0a2599bb84aa06ed53cfe7062f8f7dc02112c08a
Maruf3088 <imaruf770@gmail.com>
2025-12-21 00:25:02 +0600

    firebase setup

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,10 @@ const firebaseConfig = {
   projectId: "sample-project-a1836",
   storageBucket: "sample-project-a1836.firebasestorage.app",
   messagingSenderId: "633538146406",
-  appId: "1:633538146406:web:9f23eb9ec65444faa73da7"
+  appId: "1:633538146406:web:9f23eb9ec65444faa73da7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
